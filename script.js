fc76b35f87583ef27ec5d4eae33e692ad4025617
Andrew <58600967+Wengo500@users.noreply.github.com>
2020-08-26 20:16:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function sum(a, b, c, d) { return a + b + c + d };
 
 const curry = (args, a = 0, b = 0) => (d = 0, c = 0) => args(a, b, c, d);
-
-
 const curriedFunction = curry(sum, 1, 2); // запоминает для вызова sum 2 параметра
 
 console.log(curriedFunction(3, 4)) // выводит 10 т.е. выполняется функционал sum(1,2,3,4).
